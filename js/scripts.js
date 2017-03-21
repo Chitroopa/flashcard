85c92b81c8c2cf586f418cfac0326f719bc47ead
Chitroopa Manikkavasagam and Jesse Hoare Evans <jesse@>
2017-03-21 14:05:53 -0700

    add toggle function and fixed hide and show bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
 $(document).ready(function() {
- $(".front").click(function(){
-   $(".front").hide();
-   $(".back").show();
+
+  $("#hidetext1").click(function(){
+    $("#front-card1").toggle();
+    $("#back-card1").toggle();
+
+  });
+  $("#front-card1").click(function(){
+    $("#back-card1").toggle();
+    $("#front-card1").toggle();
+  });
+  $("#hidetext2").click(function(){
+    $("#front-card2").toggle();
+    $("#back-card2").toggle();
+
+  });
+  $("#front-card2").click(function(){
+    $("#back-card2").toggle();
+    $("#front-card2").toggle();
+  });
+ $("#hidetext3").click(function(){
+   $("#front-card3").toggle();
+   $("#back-card3").toggle();
+
  });
- $(".back").click(function(){
-   $(".back").hide();
-   $(".front").show();
+ $("#front-card3").click(function(){
+   $("#back-card3").toggle();
+   $("#front-card3").toggle();
  });
+
+
+
+
+
+
 });
